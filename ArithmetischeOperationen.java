ecd5df90106dbec9bb07dad763bcb7aa51fc7d2a
DanielGersdorf <s52035@edu.campus02.at>
2022-09-24 12:02:31 +0200

    ArithmetischeOperatoren und Calculator example Post und Pre inkremment implementiert

--- ArithmetischeOperationen.java
+++ ArithmetischeOperationen.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class ArithmetischeOperationen {
     public static void main(String[] args) {
 
@@ -14,6 +16,17 @@ public class ArithmetischeOperationen {
         //dividieren
         System.out.println(a + " / " + b + " = " + (a/ (1.0 * b)));
         System.out.println(a + " % " + b + " = " + (a%b)) ;
+
+        //a++
+        System.out.println("a++");
+        //ich erwarte mir 3
+        System.out.println(a++);
+        System.out.println("a = " + a);
+
+        //++b
+        System.out.println("++b");
+        System.out.println(++b);
+        System.out.println("b =" + ++b);
 
 
 
